c5de368d01a00d4adc8287bc0ad236a9c4818188
doncasanova <ethanjuan@yahoo.com>
2018-04-09 05:56:14 -0500

    working on count down time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,3 +30,36 @@ $("#start").click(function(){
 $('.choice:contains("guess1")').on('click',function(){
   $("#test").attr("src","animated-gifs-ghosts-16.gif");
 });
+
+//------------------------------------------------------------------------------------------------------
+//timer
+// var count = document.getElementById('timeToGo');
+// timeoutfn = function(){
+//       count.innerHTML = parseInt(count.innerHTML) - 1;
+
+// };
+// setTimeout(timeoutfn, 1000);
+
+// $.each(
+//   $('#timeToGo'), function(el) {
+//     setInterval( function() { 
+//       $(this).text($(this).text()*1 -1);
+//     }, 1000);
+//     }
+//   }
+// );
+var countDown = function() {
+  var ct = 15;
+  var $elem = $(this);
+  var display = function() {
+    $elem.text(ct--);
+  }
+  var iv = setInterval(function() {
+    display();
+    if (ct === 0) {
+      clearInterval(iv);
+    }
+  }, 1000);
+  display();
+};
+$('#timeToGo').each(countDown);
